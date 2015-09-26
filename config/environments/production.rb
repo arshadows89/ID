da517ffd4c901feb808d1a3d4af66fcdd7b5ab14
Cameron Snyder <Dooffycom@gmail.com>
2015-09-26 01:17:03 -0400

    test for heroku 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+}
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
