b4679537e191a8ccaedb2baba9d57f564aee67ef
Cameron Snyder <Dooffycom@gmail.com>
2015-12-22 13:00:25 -0500

    forums will now sort properly, if comment is updated it will update the post.updated_at also so that the newest post or comment will be on top

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :post
 
+	after_save { Post.find(self.post.id).update(:updated_at => Time.now) }
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,27 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	belongs_to :post
 
+	validates_length_of :title, :minimum => 5, :maximum => 50, :allow_blank => false
+
 	def self.all_ordered_by_child
-    	includes(:comments).order('comments.updated_at DESC')
+    		order('updated_at DESC')
 	end
 
 end
+
+# sort by posts...
+# when a comment is updated it needs to update posts updated_at....
+
+# 					 number one....
+
+# 2015-12-21 22:05:08  mess things up 24
+
+# 2015-12-22 01:52:38  mess things dasjlkdajsldas
+
+# 2015-12-22 01:52:00  mess things up 25
+
+# 2015-12-21 21:22:43  new top
+
+# 2015-12-21 20:13:22  2nd
+
+# so the issue is if you upadte post it doesnt recgnoize that
