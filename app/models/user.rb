65d1225033dd655a23249da44fdfa50003f198b5
Cameron Snyder <Dooffycom@gmail.com>
2016-01-10 01:19:48 -0500

    styled edit user page with avatar validations working for edit (untested for new)

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 	# has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
  #  	validates_attachment :avatar,
  #  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
-
+	after_validation { self.errors.messages.delete(:avatar) }
     has_attached_file :avatar,
     :default_url => 'missing.png',
     :storage => :s3,
